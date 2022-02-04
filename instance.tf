f384be57eff2dbf190801f3e3574063f53c00ccf
sanoopkc <94333137+sanoopkc@users.noreply.github.com>
2022-02-04 11:12:53 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-2"
+  access_key=var.AWS_ACCESS_KEY
+  secret_key=var.AWS_SECRET_KEY 
+}
+resource "aws_instance" "web" {
+  ami           = "ami-04902260ca3d33422"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "sanlab11-HelloWorld"
+  }
+}
